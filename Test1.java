b0f3593819726c5310ae6a3bae49a8b180946dec
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-02-14 19:32:24 +0530

    initial commit

---
+++ Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+  public static void main(String[] args) {
+    
+    System.out.println("Hello world");  // Output user input
+    System.out.println("Radhe radhe");  // Output user input
+    
+
+}}
